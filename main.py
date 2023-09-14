9b12821dd4fa2e7d22167ca40af22eb28824512f
Ankush Singh <111374890+osamaisback11@users.noreply.github.com>
2023-09-14 15:10:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -37,23 +37,50 @@ def transform_text(text):
 tfidf = pickle.load(open('vectorizer.pkl','rb'))
 model = pickle.load(open('model.pkl','rb'))
 
-st.title("Email/SMS Spam Classifier")
+# st.title("Email/SMS Spam Classifier")
 
-input_sms = st.text_area("Enter the message")
+# input_sms = st.text_area("Enter the message")
 
-if st.button('Predict', type = 'primary'):
+# if st.button('Predict', type = 'primary'):
 
-    # 1. preprocess
-      transformed_sms = transform_text(input_sms)
-    # 2. vectorize
-      vector_input = tfidf.transform([transformed_sms])
-    # 3. predict
-      result = model.predict(vector_input)[0]
-    # 4. Display
-      if result == 1:
-          st.header("Spam")
-      else:
-          st.header("Not Spam")
-if st.button('Clear'):
-    input_sms.empty()
-     
+#     # 1. preprocess
+#       transformed_sms = transform_text(input_sms)
+#     # 2. vectorize
+#       vector_input = tfidf.transform([transformed_sms])
+#     # 3. predict
+#       result = model.predict(vector_input)[0]
+#     # 4. Display
+#       if result == 1:
+#           st.header("Spam")
+#       else:
+#           st.header("Not Spam")
+# elif st.button('Clear'):
+#     input_sms.empty()
+def main():
+    st.title("Spam Classification App")
+    st.sidebar.header("User Input")
+
+    # Create a text input field for the user to enter SMS text
+    input_sms = st.text_area("Enter SMS text here:", "")
+
+    if st.button('Predict', type='primary'):
+        # 1. Preprocess
+        transformed_sms = transform_text(input_sms)
+        
+        # 2. Vectorize
+        vector_input = tfidf.transform([transformed_sms])
+        
+        # 3. Predict
+        result = model.predict(vector_input)[0]
+        
+        # 4. Display
+        if result == 1:
+            st.header("Spam")
+        else:
+            st.header("Not Spam")
+    
+    elif st.button('Clear'):
+        input_sms.empty()
+
+if __name__ == '__main__':
+    main()     
